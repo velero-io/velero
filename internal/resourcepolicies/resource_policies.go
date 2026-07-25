da3d0de31a804520fe4625e3186350fd6836a8a6
Adam Zhang <adam.zhang@broadcom.com>
2026-07-25 11:28:21 +0800

    update change log
    Signed-off-by: Adam Zhang <adam.zhang@broadcom.com>

--- internal/resourcepolicies/resource_policies.go
+++ internal/resourcepolicies/resource_policies.go
@@ -109,6 +109,7 @@ func validatePolicyLabelSelector(s *PolicyLabelSelector) error {
 	_, err := SelectorFromPolicyLabelSelector(s)
 	return err
 }
+
 // ResourceFilter defines a filter for specific resource kinds.
 type ResourceFilter struct {
 	Kinds            []string               `yaml:"kinds"`
