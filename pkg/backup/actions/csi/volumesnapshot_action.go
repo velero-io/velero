0b934260e91da111d8644a827956d4cd699c48e5
samay43 <samayrbhat43@gmail.com>
2026-08-08 12:25:48 +0530

    thread context through CleanupVolumeSnapshot and SetVolumeSnapshotContentDeletionPolicy
    Signed-off-by: samay43 <samayrbhat43@gmail.com>

--- pkg/backup/actions/csi/volumesnapshot_action.go
+++ pkg/backup/actions/csi/volumesnapshot_action.go
@@ -119,7 +119,9 @@ func (p *volumeSnapshotBackupItemAction) Execute(
 
 	vsc, err := csi.GetVSCForVS(ctx, vs, p.crClient)
 	if err != nil {
-		csi.CleanupVolumeSnapshot(vs, p.crClient, p.log)
+		// context.TODO() is used here because the BackupItemAction plugin
+		// interface's Execute() does not accept a context.Context.
+		csi.CleanupVolumeSnapshot(context.TODO(), vs, p.crClient, p.log)
 		return nil, nil, "", nil, errors.WithStack(err)
 	}
 
